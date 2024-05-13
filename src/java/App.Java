686008113da57d436e29dd2dae44338f3e354f79
ATHIEPPPP <athifzaki29@gmail.com>
2024-05-13 23:28:39 +0800

    App.Java

---
+++ src/java/App.Java
@@ -0,0 +1,22 @@
+package com.tubes.tugasbesarpbo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setTitle("Aplikasi Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
